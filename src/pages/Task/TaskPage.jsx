02aa8a15fb2395c75ff8938298df2c92b85d8168
Stiven Amaya <stivenasegura@gmail.com>
2023-01-29 22:12:12 -0500

    Sesion 11: Estructura proyecto y < npm i axios >  (finalizada)

---
+++ src/pages/Task/TaskPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TasklistComponent from "../../components/container/task_list";
+
+const TaskPage = (props) => {
+  return (
+    <div>
+      <TasklistComponent></TasklistComponent>
+    </div>
+  );
+};
+
+export default TaskPage;
